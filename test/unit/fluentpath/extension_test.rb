3b656a073254d76c501851be3a305685f6920122
Jason Walonoski <jwalonoski@mitre.org>
2016-06-23 12:40:20 -0400

    Remove binding.prys from test.

--- test/unit/fluentpath/extension_test.rb
+++ test/unit/fluentpath/extension_test.rb
@@ -30,37 +30,31 @@ class ExtensionTest < Test::Unit::TestCase
 
   def test_extension
     result = FluentPath.evaluate('Patient.extension',PATIENT)
-    binding.pry if result!=EXT_ARRAY
     assert result==EXT_ARRAY, 'Failed to resolve extension.'
   end
 
   def test_extension_with_block
     result = FluentPath.evaluate("Patient.extension('http://hl7.org/fhir/StructureDefinition/us-core-race')",PATIENT)
-    binding.pry if result!=EXT
     assert result==EXT, 'Failed to resolve extension by name.'
   end
 
   def test_extension_item
     result = FluentPath.evaluate('Patient.extension[0]',PATIENT)
-    binding.pry if result!=EXT
     assert result==EXT, 'Failed to resolve extension by index.'
   end
 
   def test_extension_value
     result = FluentPath.evaluate('Patient.extension[0].value',PATIENT)
-    binding.pry if result!=CONCEPT
     assert result==CONCEPT, 'Failed to resolve extension value.'
   end
 
   def test_extension_with_block_value
     result = FluentPath.evaluate("Patient.extension('http://hl7.org/fhir/StructureDefinition/us-core-race').value",PATIENT)
-    binding.pry if result!=CONCEPT
     assert result==CONCEPT, 'Failed to resolve named extension value.'
   end
 
   def test_extension_missing
     result = FluentPath.evaluate("Patient.extension('http://hl7.org/fhir/StructureDefinition/us-core-ethnicity')",PATIENT)
-    binding.pry if !result.nil?
     assert result==nil, 'Failed to resolve missing extension.'
   end
 
