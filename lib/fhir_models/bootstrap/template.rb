4cb0d6b0d40ab294b10c78c69f5f07c01f105c9b
Jason Walonoski <jwalonoski@mitre.org>
2016-12-09 08:29:24 -0500

    Fix generation -- missing search params.

--- lib/fhir_models/bootstrap/template.rb
+++ lib/fhir_models/bootstrap/template.rb
@@ -82,7 +82,7 @@ module FHIR
             s[-1] = s[-1][0..-2] # remove the trailing comma
             s << "#{space}}"
           else
-            s << "#{space}#{constant.upcase} = #{value}"
+            s << "#{space}#{constant.upcase} = #{value.to_s.tr!('"','\'')}"
           end
         end
         s << ''
